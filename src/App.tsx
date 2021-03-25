a53e5c1ec3572683d62f60ea67319e101f6b1a76
ycc4477 <ycc4477@gmail.com>
2021-03-25 23:58:09 +0900

    feat: 내 정보 출력

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import imgA from './assets/avatar.png';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="profile">
+                <div className="avatar-container">
+                    <img className="avatar" src={imgA} />
+                </div>
+                <div className="user-info">
+                    <div>
+                        Truly-Sparkle
+                    </div>
+                    <div>
+                        안녕하세요.
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
